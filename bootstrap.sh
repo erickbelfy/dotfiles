22188b4efb7caebe332c194078a634b12a7ecef5
Erick Belfort <ebelfort@wasabi.com>
2024-08-14 16:32:39 -0400

    update zshrc copy reference

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 rm $HOME/.zshrc
 rm $HOME/.npmrc
 rm $HOME/.gitconfig
-ln -s $HOME/dotfiles/.bashrc $HOME/.bashrc
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/.npmrc $HOME/.npmrc
 ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/.gitignore_global $HOME/.gitignore_global
